e8bf79992ecba7041cc1d0eac2ac418c4f646a64
Jhayboy <mbamarajustice1@gmail.com>
2023-02-17 14:36:33 +0100

    pool

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -10,12 +10,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAoB9UEbioqSyKNPG8b2ec_qjjVhxVPtTA",
-  authDomain: "pinetwork-8eb87.firebaseapp.com",
-  projectId: "pinetwork-8eb87",
-  storageBucket: "pinetwork-8eb87.appspot.com",
-  messagingSenderId: "370627107411",
-  appId: "1:370627107411:web:32c5284793bbec9e8284a6"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
